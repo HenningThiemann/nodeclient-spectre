50dcbb72100e6bc250bc5946325cad7bf6cf561c
Henning Thiemann <henning.thiemann@gmail.com>
2018-03-20 13:55:52 +0100

    Initial commit

---
+++ src/SpectreClient.js
@@ -0,0 +1,59 @@
+const request = require('request-promise-native');
+
+module.exports = class SpectreClient {
+    constructor(spectreURL) {
+        this.spectreURL = spectreURL;
+    }
+
+    createTestrun(projectName, suiteName) {
+        let spectre_url_post = this.spectreURL + '/runs';
+
+        let formData = {
+            project: projectName,
+            suite: suiteName
+        };
+
+        return request({
+            method: 'POST',
+            uri: spectre_url_post,
+            formData: formData,
+        })
+            .then(JSON.parse)
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    submitScreenshot(test_name, browser, size, screenshot, run_id, crop_area = '', source_url = '', fuzz_level = '', highlight_color = '') {
+        //format spectre_url to send post request to /tests
+        let spectre_url_post = this.spectreURL + '/tests';
+
+        const formData = {
+            'test[run_id]': run_id.toString(),
+            'test[name]': test_name,
+            'test[browser]': browser,
+            'test[size]': size.toString(),
+            'test[crop_area]': crop_area,
+            'test[source_url]': source_url,
+            'test[fuzz_level]': fuzz_level,
+            'test[highlight_color]': highlight_color,
+            'test[screenshot]': {
+                options: {
+                    filename: 'temp.png',
+                    contentType: 'image/png'
+                },
+                value: new Buffer(screenshot, 'base64')
+            }
+        };
+
+        return request({
+            method: 'POST',
+            uri: spectre_url_post,
+            formData: formData,
+        })
+            .then(JSON.parse)
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+};
